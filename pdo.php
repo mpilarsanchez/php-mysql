b12d8e4b82e22edf67ad665b153b0076b3e73761
mpilarsanchez <50201811+mpilarsanchez@users.noreply.github.com>
2019-07-11 16:31:11 -0300

    tercer commit

--- pdo.php
+++ pdo.php
@@ -1,10 +1,15 @@
 <?php
 function abrirBaseDeDatos(){
-   $dsn = 'mysql:dbname=movies_db;host=localhost;port=3306';
-   $username = 'root';
-   $password =  '';
-   $db= new PDO($dsn, $username, $password);
-   return $db;
+   try {
+     $dsn = 'mysql:dbname=movies_db;host=localhost;port=3306';
+     $username = 'root';
+     $password =  '';
+     $db= new PDO($dsn, $username, $password);
+     //$db->​setAttribute(PDO::ATTRR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     return $db;
+   } catch (\Exception $e) {
+      echo $e->getMessage();
+   }
 }
-
  ?>
